2f8b111f410ba9eb3828f574c3a00fe5267c5a74
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-12 21:45:05 +0530

    Initial Commit

---
+++ ColapsableHeader/ColapsableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColapsableHeaderApp.swift
+//  ColapsableHeader
+//
+//  Created by shehan karunarathna on 2022-02-12.
+//
+
+import SwiftUI
+
+@main
+struct ColapsableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
